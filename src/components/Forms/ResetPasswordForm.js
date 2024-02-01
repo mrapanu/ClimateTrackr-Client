0439d0452b395e27f0f3feec9119b93971946ba7
mrapanu <mrapanu@gmail.com>
2024-02-01 11:19:28 +0200

    Added functionality for Reset User Password form.

--- src/components/Forms/ResetPasswordForm.js
+++ src/components/Forms/ResetPasswordForm.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { changeUserPasswordAsync } from "../../util/api";
+import { jwtDecode } from "jwt-decode";
+import { Ctx } from "../../util/reducer";
 import "./ResetPasswordForm.css";
 
 const ResetPasswordForm = () => {
+  const { state } = useContext(Ctx);
   const [currentPw, setCurrentPw] = useState("");
   const [newPw, setNewPw] = useState("");
   const [confirmNewPw, setConfirmNewPw] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageError, setMessageError] = useState("");
+  const jwt = jwtDecode(localStorage.getItem("token"));
+
+  const handleChangePassword = () => {
+    if (newPw !== confirmNewPw) {
+      setMessageError('"New Password" and "Confirm Password" do not match.');
+    } else {
+      changeUserPasswordAsync(
+        jwt.unique_name,
+        currentPw,
+        newPw,
+        state.url,
+        setMessage,
+        setMessageError
+      );
+    }
+    setConfirmNewPw("");
+    setNewPw("");
+    setCurrentPw("");
+    const timeoutId = setTimeout(() => {
+      setMessageError("");
+      setMessage("");
+    }, 5000);
+    return () => clearTimeout(timeoutId);
+  };
+
   return (
     <div className="reset-password-container">
       <div className="resetpw-title">CHANGE PASSWORD</div>
@@ -36,10 +67,16 @@ const ResetPasswordForm = () => {
         />
       </div>
       <div className="buttons-pw-container">
-        {currentPw !== "" && newPw !== "" && confirmNewPw != "" && (
-          <button className="button-resetpw">Change Password</button>
+        {currentPw !== "" && newPw !== "" && confirmNewPw !== "" && (
+          <button className="button-resetpw" onClick={handleChangePassword}>
+            Change Password
+          </button>
         )}
       </div>
+      {messageError !== "" && (
+        <div className="resetpw-error">{messageError}</div>
+      )}
+      {message !== "" && <div className="resetpw-success">{message}</div>}
     </div>
   );
 };
