324dd53b7a6113d66ba368f918f7ac4669f2957d
mrapanu <mrapanu@gmail.com>
2024-02-01 11:18:39 +0200

    Implement functionality for Profile Info Form (update FullName and Email for user)

--- src/components/Forms/ProfileInfoForm.js
+++ src/components/Forms/ProfileInfoForm.js
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { Ctx } from "../../util/reducer";
+import { updateProfileAsync } from "../../util/api";
 import "./ProfileInfoForm.css";
 
 const ProfileInfoForm = () => {
+  const { state, dispatch } = useContext(Ctx);
   const [inputFn, setInputFn] = useState(false);
-  const [inputFnValue, setInputFnValue] = useState("");
-  const [inputEmailValue, setInputEmailValue] = useState("");
   const [inputEmail, setInputEmail] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageErr, setMessageErr] = useState("");
+  const [inputFnValue, setInputFnValue] = useState(state.userProfile.fullName);
+  const [inputEmailValue, setInputEmailValue] = useState(
+    state.userProfile.email
+  );
 
-  const handleFnChange = (e) => {
-    setInputFnValue(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setInputFnValue(e.target.value);
+  const handleUpdateProfile = () => {
+    updateProfileAsync(
+      inputEmailValue,
+      inputFnValue,
+      state.url,
+      setMessage,
+      setMessageErr,
+      dispatch
+    );
+    setInputEmail(false);
+    setInputFn(false);
+    const timeoutId = setTimeout(() => {
+      setMessageErr("");
+      setMessage("");
+    }, 5000);
+    return () => clearTimeout(timeoutId);
   };
 
   return (
@@ -20,7 +37,7 @@ const ProfileInfoForm = () => {
       <div className="pf-info-title">PROFILE INFO</div>
       <div className="info-row-container">
         <div className="pf-info-description">Username:</div>
-        <div className="pf-info-value">mihai</div>
+        <div className="pf-info-value">{state.userProfile.username}</div>
       </div>
       <div className="info-row-container">
         <div className="pf-info-description">Full Name:</div>
@@ -29,13 +46,13 @@ const ProfileInfoForm = () => {
             type="text"
             className="pf-info-input"
             value={inputFnValue}
-            onChange={handleFnChange}
+            onChange={(e) => setInputFnValue(e.target.value)}
           />
         ) : (
           <input
             type="text"
             className="pf-info-input"
-            placeholder="Mihai lala"
+            placeholder={inputFnValue !== "" ? inputFnValue : "Not Set"}
             readOnly
             onClick={() => setInputFn(true)}
           />
@@ -49,13 +66,13 @@ const ProfileInfoForm = () => {
             type="text"
             className="pf-info-input"
             value={inputEmailValue}
-            onChange={handleEmailChange}
+            onChange={(e) => setInputEmailValue(e.target.value)}
           />
         ) : (
           <input
             type="text"
             className="pf-info-input"
-            placeholder="mihai@gmail.com"
+            placeholder={inputEmailValue !== "" ? inputEmailValue : "Not Set"}
             readOnly
             onClick={() => setInputEmail(true)}
           />
@@ -73,10 +90,16 @@ const ProfileInfoForm = () => {
             >
               Cancel
             </button>
-            <button className="button-save">Save</button>
+            <button className="button-save" onClick={handleUpdateProfile}>
+              Save
+            </button>
           </>
         )}
       </div>
+      {messageErr !== "" && (
+        <div className="updateprofile-error">{messageErr}</div>
+      )}
+      {message !== "" && <div className="updateprofile-success">{message}</div>}
     </div>
   );
 };
